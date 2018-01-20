3d94f0ad3853d45904a77218a6d025667c7af456
Will Stone <Willstone109@aol.com>
2018-01-20 15:30:17 -0500

    extract redundancy and prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,49 +1,55 @@
 class ArticlesController < ApplicationController
-def index
-  @articles = Article.all
-end
-def new
-  @article = Article.new
-end
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
-def edit
-  @article = Article.find(params[:id])
-end
-
-def create
-  @article = Article.new(article_params)
-  if @article.save
-   flash[:notice] = "Article was successfully"
-   redirect_to article_path(@article)
-  else
-  render 'new'
+  def index
+    @articles = Article.all
   end
-end
 
-
-def update
-  @article = Article.find(params[:id])
-  if @article.update(article_params)
-    flash[:notice] = "Article was successfully updated"
-    redirect_to article_path(@article)
-  else
-    render 'edit'
+  def new
+    @article = Article.new
   end
-end
+
+  def edit
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was successfully"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+
+  def update
+
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
   def show
-    @article = Article.find(params[:id])
+
   end
 
   def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
-    flash[:notice] = "Article was successfully deleted"
-    redirect_to articles_path
+      @article.destroy
+      flash[:notice] = "Article was successfully deleted"
+      redirect_to articles_path
   end
 
 
 
 private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
   def article_params
       params.require(:article).permit(:title, :description)
   end
